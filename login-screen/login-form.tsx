14d3ff3f2b3d8e2f1c226626f17197f3f0f64a87
Guy Wolf <guywolf2@gmail.com>
2023-09-17 09:32:29 +0300

    💄 Make component look more like Figma

--- login-screen/login-form.tsx
+++ login-screen/login-form.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { Image } from "react-native";
+import { Image } from "expo-image";
 import { Surface, TextInput, Button } from "react-native-paper";
 import submitForm from "./login-submit";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  
   return (
-    <Surface style={{backgroundColor: "#fff"}}>
+    <Surface style={{ backgroundColor: "#fff" }} elevation={0}>
       <Image
         source={require("./assets/mada_logo.png")}
-        style={{ marginBottom: 50 }}
+        style={{ marginBottom: 50, height: 119, width: 224, margin: 30 }}
       />
       <TextInput
         label="שם משתמש"
@@ -21,6 +21,7 @@ export default function LoginForm() {
         textAlign="right"
         outlineColor="#ff0000"
         activeOutlineColor="#ff0000"
+        style={{ marginBottom: 20 }}
       />
       <TextInput
         label="סיסמה"
@@ -31,8 +32,15 @@ export default function LoginForm() {
         textAlign="right"
         outlineColor="#ff0000"
         activeOutlineColor="#ff0000"
+        style={{ marginBottom: 20 }}
       />
-      <Button style={{backgroundColor: "#ff0000", borderRadius: 4}} textColor="black" onPress={submitForm}>התחברות</Button>
+      <Button
+        style={{ backgroundColor: "#ff0000", borderRadius: 4, padding: 5 }}
+        textColor="black"
+        onPress={submitForm}
+      >
+        התחברות
+      </Button>
     </Surface>
   );
 }
