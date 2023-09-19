b7fa2405a3d61d5710b62a5e8d720bdd368ea6c8
Guy Wolf <guywolf2@gmail.com>
2023-09-19 10:41:07 +0300

    🎉Add a basic login screen (#1)

---
+++ login-screen/login-form.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Image } from "expo-image";
+import { StyleSheet } from "react-native";
+import { Surface, Button } from "react-native-paper";
+import submitForm from "./login-submit";
+import { FormField } from "../common/form-components";
+
+export default function LoginForm() {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    return (
+        <Surface style={styles.form} elevation={0}>
+            <Image
+                source={require("./assets/mada_logo.png")}
+                style={{
+                    marginBottom: 50,
+                    height: 119,
+                    width: 224,
+                    margin: 30,
+                }}
+            />
+            <FormField
+                formDataState={{ data: username, setData: setUsername }}
+                label="שם משתמש"
+            />
+            <FormField
+                formDataState={{ data: password, setData: setPassword }}
+                label="סיסמה"
+                isPassword
+            />
+            <Button
+                style={{
+                    backgroundColor: "#ff0000",
+                    borderRadius: 4,
+                    padding: 5,
+                }}
+                textColor="black"
+                onPress={() => submitForm(username, password)}
+            >
+                התחברות
+            </Button>
+        </Surface>
+    );
+}
+
+const styles = StyleSheet.create({
+    form: {
+      flex: 1,
+      backgroundColor: "#fff",
+      borderColor: "#ff0000",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
