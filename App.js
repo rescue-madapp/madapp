6ad925a86adce101c0441742dc860028731dd4e5
Guy Wolf <guywolf2@gmail.com>
2023-09-26 21:47:37 +0300

    💄 Fix weird UI uncenter issue

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import LoginForm from "./login-screen/login-form";
 
 export default function App() {
     return (
-        <View>
+        <View style={styles.form}>
             <StatusBar />
             <LoginForm />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    form: {
+        flex: 1,
+        backgroundColor: "#fff",
+        borderColor: "#ff0000",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
