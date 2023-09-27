3c36dc78b2b9ad2ab3dec89801862073333a3d7b
Guy Wolf <guywolf2@gmail.com>
2023-09-27 15:50:31 +0300

    ✨ Add working menu with 3 pages

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import LoginForm from "./login-screen/login-form";
-import MainForm from "./main-screen/main-form";
 import LogEvent from "./log-event/log-event"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RegisterForm from "./register-screen/register-form";
 import LoginForm from "./login-screen/login-form";
+import ProfileForm from "./profile-page/profile-form";
+import MainScreen from "./main-screen/main-screen";
 
 const stack = createNativeStackNavigator();
 
@@ -15,6 +15,8 @@ export default function App() {
         <NavigationContainer>
           <StatusBar />
           <stack.Navigator>
+            <stack.Screen name="Main" component={MainScreen} options={{headerShown: false}} />
+            <stack.Screen name="Profile" component={ProfileForm} options={{headerShown: false}} />
             <stack.Screen name="Log Event" component={LogEvent} options={{headerShown: false}} />
             <stack.Screen name="Login" component={LoginForm} options={{headerShown: false}} />
             <stack.Screen name="Register" component={RegisterForm} options={{headerShown: false}} />
--- menu/menu.tsx
+++ menu/menu.tsx
@@ -1,30 +1,47 @@
 import { useState } from "react";
 import { IconButton, Surface } from "react-native-paper";
 import { BottomNavigation, Text } from "react-native-paper";
-import LoginForm from "../login-screen/login-form";
+import MainForm from "../main-screen/main-form";
+import ProfileForm from "../profile-page/profile-form";
+import LogEvent from "../log-event/log-event";
 
-const HomeRoute = () => <Text>hi</Text>;
+const MainRoute = () => <MainForm />;
+const ProfileRoute = () => <ProfileForm />;
+const LogRoute = () => <LogEvent />;
 
 export function BottomMenu() {
     const [index, setIndex] = useState(0);
     const [routes] = useState([
         {
-            key: "home",
-            title: "דף הבית",
+            key: "main",
+            title: "בית",
             focusedIcon: "home",
             unfocusedIcon: "home-outline",
         },
+        {
+            key: "profile",
+            title: "פרופיל",
+            focusedIcon: "account",
+            unfocusedIcon: "account-outline",
+        },
+        {
+            key: "log",
+            title: "הזן מפגש",
+            focusedIcon: "pencil",
+            unfocusedIcon: "pencil-outline",
+        },
     ]);
     const renderScene = BottomNavigation.SceneMap({
-        home: HomeRoute,
+        main: MainRoute,
+        profile: ProfileRoute,
+        log: LogRoute,
     });
     return (
         <BottomNavigation
-            navigationState={{ index, routes }}
+            navigationState={{ index: index, routes: routes }}
             onIndexChange={setIndex}
             renderScene={renderScene}
             compact={false}
-            style={{ width: "100%", paddingTop: 0 , marginTop:0}}
         />
     );
 }
