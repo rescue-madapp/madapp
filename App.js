af74eb7965fb279190370ee12463844be2e0630c
Guy Wolf <guywolf2@gmail.com>
2023-09-27 14:53:45 +0300

    💄 Touch up UI

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import LoginForm from "./login-screen/login-form";
 import LogEvent from "./log-event/log-event"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -14,6 +13,7 @@ export default function App() {
         <NavigationContainer>
           <StatusBar />
           <stack.Navigator>
+            <stack.Screen name="Log Event" component={LogEvent} options={{headerShown: false}} />
             <stack.Screen name="Login" component={LoginForm} options={{headerShown: false}} />
             <stack.Screen name="Register" component={RegisterForm} options={{headerShown: false}} />
             </stack.Navigator>
